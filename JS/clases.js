dd45b3634bb35e7c242302b7c9bb29286c788aed
AlvMur <alvmurapa@gmail.com>
2021-02-02 18:45:55 +0100

    Compra y Subscripcion

--- JS/clases.js
+++ JS/clases.js
@@ -8,4 +8,54 @@ class Juego{
         this.precio = precio;
         this.pegi = pegi;
     }   
+
+
+    
+
+}
+
+class Compra{
+
+    constructor(sIdCompra, sIdCliente, sIdJuego, dFecha, dCosteCompra){
+
+        this.idCompra = sIdCompra;
+        this.idCliente = sIdCliente;
+        this.idJuego = sIdJuego;
+        this.fecha = dFecha;
+        this.costeCompra = dCosteCompra;
+
+
+    }
+
+}
+Compra.prototype.toHTMLRow = function (){
+    let sFila = "<tr>";
+    sFila +="<td>" + this.idCompra + "</td>";
+    sFila +="<td>" + this.idCliente + "</td>";
+    sFila +="<td>" + this.idJuego + "</td>";
+    sFila +="<td>" + this.fecha + "</td>";
+    sFila +="<td>" + this.costeCompra + "</td>";
+
+    return sFila;
+}
+
+class Subscripcion{
+
+    constructor(sIdSubscripcion, dFechaExp, dPrecio){
+
+        this.idSubscripcion = sIdSubscripcion;
+        this.fechaExp = dFechaExp;
+        this.precio = dPrecio;
+    }
+}
+
+Subscripcion.prototype.toHTMLRow = function(){
+
+    let sFila = "<tr>";
+    sFila +="<td>" + this.idSubscripcion + "</td>";
+    sFila +="<td>" + this.fechaExp + "</td>";
+    sFila +="<td>" + this.precio + "</td>";
+   
+
+    return sFila;  
 }
