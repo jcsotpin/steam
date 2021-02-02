48941061fc19f54a639d0b3d44d533a49e20c119
Vinaza-24 <73523496+Vinaza-24@users.noreply.github.com>
2021-02-02 18:37:18 +0100

    Get and set Juego

--- JS/clases.js
+++ JS/clases.js
@@ -8,7 +8,50 @@ class Juego {
         this.precio = precio;
         this.pegi = pegi;
     }
+    //Id Juego
+    getId_juego() {
+        return this.id_juego;
+    }
+    setId_juego(id_juego){
+        this.id_juego= id_juego;
+    }
+    //Titulo
+    getTitulo() {
+        return this.titulo;
+    }
+    setTitulo(titulo){
+        this.titulo= titulo;
+    }
+    //Genero
+    getGenero() {
+        return this.id_juego;
+    }
+    setGenero(genero){
+        this.genero= genero;
+    }
+    //Año lanzamiento
+    getAño_lanzamiento() {
+        return this.año_lanzamiento;
+    }
+    setAño_lanzamiento(genero){
+        this.año_lanzamiento= año_lanzamiento;
+    }
+    //Precio
+    getPrecioo() {
+        return this.precio;
+    }
+    setPrecio(precio){
+        this.precio= precio;
+    }
+        //Pegi
+        getPegi() {
+        return this.pegi;
+    }
+    setPegi(pegi){
+        this.pegi= pegi;
+    }
 }
+
 class Cliente {
     constructor(iID, sNombre, sApellidos, dFecha, sEmail) {
 
