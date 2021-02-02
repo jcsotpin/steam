bbd849fce28eb6a236f7396f697cb7fbcb73641f
Vinaza-24 <73523496+Vinaza-24@users.noreply.github.com>
2021-02-02 18:00:09 +0100

    Clase Juego

--- codigo.js
+++ codigo.js
@@ -18,4 +18,14 @@ function mostrarAltaSubcripcion() {
     formAltaSubcripcion.style.display = "block";
 }
 
+//Juego
 
+function Juego (id_juego, titulo, genero, año_lanzamiento, precio, pegi){
+
+    this.id_juego = id_juego;
+    this.titulo = titulo;
+    this.genero = genero;
+    this.año_lanzamiento = año_lanzamiento;
+    this.precio = precio;
+    this.pegi = pegi;
+}
