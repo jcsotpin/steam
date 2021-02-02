6dd7abb7047c3fae9cb59b6029e6d41d4fa14f11
Vinaza-24 <73523496+Vinaza-24@users.noreply.github.com>
2021-02-02 18:10:36 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -18,16 +18,4 @@ function mostrarAltaSubcripcion() {
     formAltaSubcripcion.style.display = "block";
 }
 
-//Juego
 
-class Juego{ 
-    constructor(id_juego, titulo, genero, año_lanzamiento, precio, pegi){
-
-        this.id_juego = id_juego;
-        this.titulo = titulo;
-        this.genero = genero;
-        this.año_lanzamiento = año_lanzamiento;
-        this.precio = precio;
-        this.pegi = pegi;
-    }   
-}
