f3f0cd044ed6d98ea094ba7961decd78557747d1
xdaraboz <73521177+xdaraboz@users.noreply.github.com>
2021-02-02 18:30:58 +0100

    cliente

---
+++ JS/clases.js
@@ -0,0 +1,67 @@
+class Cliente {
+    constructor(iID, sNombre, sApellidos, dFecha, sEmail) {
+
+        this.iId = iID;
+        this.sNombre = sNombre;
+        this.sApellidos = sApellidos;
+        this.dFecha = dFecha;
+        this.sEmail = sEmail;
+
+    }
+
+    getID() {
+        return this.iId;
+    }
+    setID(iID) {
+        this.iId = iID;
+    }
+    getNombre() {
+        return this.sNombre;
+    }
+    setNombre(sNombre) {
+        this.sNombre = sNombre;
+    }
+    getApellidos() {
+        return this.sApellidos;
+    }
+    setApellidos(sApellidos) {
+        this.sApellidos = sApellidos;
+    }
+    getFecha() {
+        return this.dFecha;
+    }
+    setFecha(dFecha) {
+        this.dFecha = dFecha;
+    }
+    getEmail() {
+        return this.sEmail;
+    }
+    setEmail(sEmail) {
+        this.sEmail = sEmail;
+    }
+
+    toHTMLRow() {
+
+        let sCadena = "";
+        sCadena += "<tr>";
+        sCadena += "<td>";
+        sCadena += this.getID();
+        sCadena += "</td>";
+        sCadena += "<td>";
+        sCadena += this.getNombre();
+        sCadena += "</td>";
+        sCadena += "<td>";
+        sCadena += this.getApellidos();
+        sCadena += "</td>";
+        sCadena += "<td>";
+        sCadena += this.getFecha();
+        sCadena += "</td>";
+        sCadena += "<td>";
+        sCadena += this.getEmail();
+        sCadena += "</td>";
+        sCadena += "</tr>";
+        return sCadena;
+
+    }
+
+}
