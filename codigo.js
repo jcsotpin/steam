5faec8729a55d34ae1188a035681a340f1abde47
Juan Carlos Sotelo <73720448+jcsotpin@users.noreply.github.com>
2021-02-02 18:05:54 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -20,12 +20,14 @@ function mostrarAltaSubcripcion() {
 
 //Juego
 
-function Juego (id_juego, titulo, genero, año_lanzamiento, precio, pegi){
+class Juego{ 
+    constructor(id_juego, titulo, genero, año_lanzamiento, precio, pegi){
 
-    this.id_juego = id_juego;
-    this.titulo = titulo;
-    this.genero = genero;
-    this.año_lanzamiento = año_lanzamiento;
-    this.precio = precio;
-    this.pegi = pegi;
+        this.id_juego = id_juego;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.año_lanzamiento = año_lanzamiento;
+        this.precio = precio;
+        this.pegi = pegi;
+    }   
 }
