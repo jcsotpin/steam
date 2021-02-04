99dc743fbd34ffb4becf536dc5707a0551c187cf
Vinaza-24 <73523496+Vinaza-24@users.noreply.github.com>
2021-02-04 19:10:57 +0100

    Validación de Formularios

--- JS/ValidacionForm.js
+++ JS/ValidacionForm.js
@@ -1,7 +1,7 @@
 "use strict";
 function valida_envia(){
         //valido el DNI
-        if (document.fvalida.txtNIF.value.length==0){
+        if (document.formAdministracion.txtNIF.value.length==0){
               alert("Tiene que escribir su DNI")
               document.fvalida.txtNIF.focus()
               return 0;
@@ -9,21 +9,21 @@ function valida_envia(){
         }
 
         //valido el nombre
-        if (document.fvalida.txtNombre.value.length==0){
+        if (document.formAdministracion.txtNombre.value.length==0){
               alert("Tiene que escribir su Nombre")
               document.fvalida.txtNombre.focus()
               return 0;
         }
 
         //valido el apellidos
-        if (document.fvalida.txtApellidos.value.length==0){
+        if (document.formAdministracion.txtApellidos.value.length==0){
               alert("Tiene que escribir su Apellido")
               document.fvalida.txtApellidos.focus()
               return 0;
         }
 
         //valido el Correo
-        if (document.fvalida.txtDireccion.value.length==0){
+        if (document.formAdministracion.txtDireccion.value.length==0){
               alert("Tiene que escribir su Correo Electronico")
               document.fvalida.txtDireccion.focus()
               return 0;
